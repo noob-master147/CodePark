71791e38c84d19efc624479d87ad4ca6fcf6b84b
noob-master147 <divyanshkhandelwal147@gmail.com>
2019-09-27 04:01:10 +0530

    click bait done

--- client/app.js
+++ client/app.js
@@ -2,27 +2,23 @@ const buttonOne = document.getElementById('buttonOne');
 const buttonTwo = document.getElementById('buttonTwo');
 const buttonThree = document.getElementById('buttonThree');
 
-const countOne = document.getElementById("countOne");
-const countTwo = document.getElementById("countTwo");
-const countThree = document.getElementById("countThree");
-
 var counterOne = 0
 var counterTwo = 0
 var counterThree = 0
 
 buttonOne.onclick = function() {
     counterOne += 1
-    countOne.innerHTML = counterOne;
+    buttonOne.innerHTML = counterOne;
 }
 
 buttonTwo.onclick = function() {
     counterTwo += 1
-    countTwo.innerHTML = counterTwo;
+    buttonTwo.innerHTML = counterTwo;
 }
 
 buttonThree.onclick = function() {
     counterThree += 1
-    countThree.innerHTML = counterThree;
+    buttonThree.innerHTML = counterThree;
 }
 
 
