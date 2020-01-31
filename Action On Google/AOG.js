6dd8efdf870692e9979f695606f43789eb26d06e
Divyansh <divyanshkhandelwal147@gmail.com>
2020-01-31 23:54:35 +0530

    current working code with all in one loop

--- Action On Google/AOG.js
+++ Action On Google/AOG.js
@@ -1,38 +1,60 @@
 const fetch = require('node-fetch')
-const tag = ['loop']
-const preUrl = 'https://api.codepark.in/topic/'
-const postUrl = '/related/questions'
-const urlList = []
-const questionList = []
-const quesName = []
-for (let index = 0; index < tag.length; index++) {
-  const element = tag[index];
-  const url = preUrl + element + postUrl
-  urlList.push(url)
-}
-const getQuestion = new Promise((resolve, reject) => {
+
+
+
+const getQuestion = (() => {
+  const tag = ['loop'];
+  const preUrl = 'https://api.codepark.in/topic/';
+  const postUrl = '/related/questions';
+  const urlList = [];
+  const questionList = [];
+  const quesName = [];
+
+  //Loop to create the api urls from tags
+  for (let index = 0; index < tag.length; index++) {
+    const element = tag[index];
+    const url = preUrl + element + postUrl;
+    urlList.push(url);
+  }
   urlList.forEach((url) => {
     fetch(url)
       .then(res => res.json())
       .then(json => {
         json.questions.forEach((obj) => {
-          const questionObj = `https://api.codepark.in/content/questions/details/${obj.uid}`
-          questionList.push(questionObj);
+          questionList.push(`https://api.codepark.in/content/questions/details/${obj.uid}`);
           quesName.push(obj.question);
         })
       })
-      .then(res => {
+      .then(() => {
         setTimeout(() => {
-          resolve()
-        }, 5000);
+        }, 3000);
+      })
+      .then(() =>{
+        console.log(questionList)
       })
   })
 })
 
-getQuestion
-  .then(res => console.log(questionList))
-  .then(res => console.log(quesName))
-  .catch(e => console.log(e))
+getQuestion()
+//   .then(res => {
+//     console.log(questionList)
+
+//   })
+//   .catch(e => console.log(e))
 
 
-  //show me top 10 questions in loop
+//show me top 10 questions in loop
+
+// agent.add();
+// agent.add(new Card({
+//   title: ` `,
+//   imageUrl: 'https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png',
+//   buttonText: 'This is a button',
+//   buttonUrl: 'https://assistant.google.com/'
+// })
+// );
+
+
+// urlList.forEach((url) => {
+
+// });
