92fd174dbbb19b7f99c4d7a05281505929804f27
Divyansh <divyanshkhandelwal147@gmail.com>
2020-02-17 21:52:35 +0530

    Version2 for backend

---
+++ Action On Google/AOG_V2.js
@@ -0,0 +1,36 @@
+const fetch = require('node-fetch')
+
+const getQuestion = () => {
+    return new Promise((resolve, reject) => {
+        const tag = ['loop']
+        const apiList = []
+        const redirectList = []
+        tag.forEach(tag => {
+            const url = `https://api.codepark.in/topic/${tag}/related/questions`
+            apiList.push(url)
+        })
+        apiList.forEach(api => {
+            fetch(api)
+                .then((res) => res.json())
+                .then((json) => json.questions)
+                .then((question) => {
+                    question.forEach(question => {
+                        const listItem = `https://www.codepark.in/question/view/${question.qname}/${question.uid}`
+                        redirectList.push(listItem)
+                        resolve(redirectList)
+                    })
+                })
+                .catch((error) => {
+                    reject(error)
+                })
+        })
+    })
+}
+
+getQuestion()
+.then((res) =>{
+    console.log(res)
+})
+.catch((err) => {
+    console.log('Encountered An Error',err)
+})
