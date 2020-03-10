231d96987a93831d6f612e9d9f4e4ef7dc23ae9a
Divyansh <divyanshkhandelwal147@gmail.com>
2020-03-11 01:32:21 +0530

    working

--- WebHook/controllers/controller.js
+++ WebHook/controllers/controller.js
@@ -3,48 +3,112 @@ const fetch = require('node-fetch')
 
 const returnQuestion = (req) => {
     return new Promise(async (resolve, reject) => {
-        try {
-            console.log(chalk.green("printing Request"))
-            const tag = req.queryResult.parameters.tag
-            const apiList = []
-            const redirectList = []
-            tag.forEach(tag => {
-                const url = `https://api.codepark.in/topic/${tag.toLowerCase()}/related/questions`
-                apiList.push(url)
-            })
-            apiList.forEach(api => {
-                fetch(api)
-                    .then((res) => res.json())
-                    .then((json) => json.questions)
-                    .then((question) => {
-                        question.forEach(question => {
-                            const listItem = {
-                                url: `https://www.codepark.in/question/view/${question.qname}/${question.uid}`,
-                                name: `${question.question}`
+
+        const tag = req.queryResult.parameters.tag
+        let apiList = []
+        let redirectList = []
+        tag.forEach(tag => {
+            const url = `https://api.codepark.in/topic/${tag.toLowerCase()}/related/questions`
+            apiList.push(url)
+        })
+        console.log(apiList)
+        apiList.forEach(api => {
+            fetch(api)
+                .then((res) => res.json())
+                .then((json) => {
+                    if (json.code == 0) {
+                        return json.questions
+                    } else {
+                        const item = [
+                            {
+                                "simpleResponse": {
+                                    "textToSpeech": `${json.message}`
+                                }
                             }
-                            redirectList.push(listItem)
-                            resolve(redirectList)
+                        ]
+                        resolve(item)
+                    }
+                })
+                .then((question) => {
+                    question.forEach(question => {
+                        const listItem = {
+                            url: `https://www.codepark.in/question/view/${question.qname}/${question.uid}`,
+                            name: `${question.question}`,
+                            answer: `${question.answers.length}`
+                        }
+                        redirectList.push(listItem)
+                    })
+                    // console.log(redirectList)
+                    
+                    return redirectList
+                })
+
+                .then((redirectObject) => {
+                    if (redirectObject.length >= 2) {
+                        const carouselBrowseItem = []
+                        redirectObject.forEach(obj => {
+                            const data = {
+                                "title": obj.name,
+                                "openUrlAction": {
+                                    "url": obj.url
+                                },
+                                "description": `Solved By ${obj.answer} Users`,
+                                "image": {
+                                    "url": "https://i.ibb.co/DVwng4F/logo.png",
+                                    "accessibilityText": "CodePark"
+                                }
+                            }
+                            carouselBrowseItem.push(data)
                         })
-                    })
-                    .catch((error) => {
-                        reject(error)
-                    })
-            })
+                        const item = [
+                            {
+                                "simpleResponse": {
+                                    "textToSpeech": "Here are few questions from CodePark"
+                                }
+                            },
+                            {
+                                "carouselBrowse": {
+                                    "items": carouselBrowseItem
+                                }
+                            }
+                        ]
+                        resolve(item)
 
-        } catch (e) {
-            console.log(chalk.red("Error in connection to controllers"))
-            console.log(e)
-            reject({
-                statusCode: 400,
-                payload: {
-                    msg: "Server Side error contact support"
-                }
-            })
-        }
-
+                    } else {
+                        const item = [
+                            {
+                                "simpleResponse": {
+                                    "textToSpeech": "Here is a matching result from CodePark"
+                                }
+                            },
+                            {
+                                "basicCard": {
+                                    "title": `${redirectObject[0].name}`,
+                                    "subtitle": `Solved By ${redirectObject[0].answer} Users`,
+                                    "image": {
+                                        "url": "https://i.ibb.co/DVwng4F/logo.png",
+                                        "accessibilityText": "CodePark"
+                                    },
+                                    "buttons": [
+                                        {
+                                            "title": "Click Here",
+                                            "openUrlAction": {
+                                                "url": redirectObject[0].url
+                                            }
+                                        }
+                                    ]
+                                }
+                            }
+                        ]
+                        resolve(item)
+                    }
+                })
+                .catch(error => reject(error))
+        })
     })
 }
 
 module.exports = {
     returnQuestion
 }
+
--- WebHook/routes/users.js
+++ WebHook/routes/users.js
@@ -7,86 +7,21 @@ const control = require('../controllers/controller');
 /* GET users listing. */
 router.post('/', (req, res) => {
   control.returnQuestion(req.body)
-    .then((redirectObject) => {
-      // console.log(redirectObject)
+    .then((items) => {
       const data = {
         "payload": {
           "google": {
             "expectUserResponse": true,
             "richResponse": {
-              "items": [
-                {
-                  "simpleResponse": {
-                    "textToSpeech": "Here are few questions from CodePark"
-                  }
-                },
-                {
-                  "carouselBrowse": {
-                    "items": [
-                      {
-                        "title": redirectObject[0].name,
-                        "openUrlAction": {
-                          "url": redirectObject[0].url
-                        },
-                        "image": {
-                          "url":"https://i.ibb.co/DVwng4F/logo.png",
-                          "accessibilityText": "CodePark"
-                        }
-                      },
-                      {
-                        "title": redirectObject[1].name,
-                        "openUrlAction": {
-                          "url": redirectObject[1].url
-                        },
-                        "image": {
-                          "url":"https://i.ibb.co/DVwng4F/logo.png",
-                          "accessibilityText": "CodePark"
-                        }
-                      },
-                      {
-                        "title": redirectObject[2].name,
-                        "openUrlAction": {
-                          "url": redirectObject[2].url
-                        },
-                        "image": {
-                          "url":"https://i.ibb.co/DVwng4F/logo.png",
-                          "accessibilityText": "CodePark"
-                        }
-                      },
-                      {
-                        "title": redirectObject[3].name,
-                        "openUrlAction": {
-                          "url": redirectObject[3].url
-                        },
-                        "image": {
-                          "url":"https://i.ibb.co/DVwng4F/logo.png",
-                          "accessibilityText": "CodePark"
-                        }
-                      },
-                      {
-                        "title": redirectObject[4].name,
-                        "openUrlAction": {
-                          "url": redirectObject[4].url
-                        },
-                        "image": {
-                          "url":"https://i.ibb.co/DVwng4F/logo.png",
-                          "accessibilityText": "CodePark"
-                        }
-                      }
-                    ]
-                  }
-                }
-              ]
+              "items": items
             }
           }
         }
       }
-
-      console.log(redirectObject[0])
       return data
     })
     .then(resp => res.status(200).send(resp))
-    .catch(err => res.status(400).send(err))
+    .catch((err) => res.status(400).send(err))
 })
 
 module.exports = router;
